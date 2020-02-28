388deb022272f466297dfa1641b7ff94a780c143
xxNpCxx <xxNpCxx@gmail.com>
2020-02-28 06:41:14 +0300

    fix invalid currency and invalid tradeday

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -20,6 +20,8 @@ use Symfony\Component\HttpFoundation\Response;
 class ApiController extends BaseController
 {
     private const  DEFAULT_CURRENCY_CODE = 'R01015';
+    private const  MAX_DAYS_RANGE = '7';
+
     private CBRClient $CBRClient;
     private int $responceStatus = Response::HTTP_OK;
 
@@ -38,9 +40,9 @@ class ApiController extends BaseController
         $date = $this->request->request->get('date')
             ? date(CBRClient::CBR_DATE_FORMAT, strtotime($this->request->request->get('date')))
             : date(CBRClient::CBR_DATE_FORMAT);
-        $prevDayDate = date(CBRClient::CBR_DATE_FORMAT, strtotime($date . '- 1 day'));
+        $prevDayDate = date(CBRClient::CBR_DATE_FORMAT, strtotime($date . '- ' . self::MAX_DAYS_RANGE . ' day'));
         $rates = $this->CBRClient->getDynamicRate($prevDayDate, $date, $code);
-        if (count($rates) == 2) {
+        if ($rates) {
             $fromDayRate = $rates[0];
             $toDayRate = $rates[1];
             $diff = $fromDayRate - $toDayRate;
@@ -69,19 +71,28 @@ class ApiController extends BaseController
         $baseCode = $this->request->request->get('baseCode', self::DEFAULT_CURRENCY_CODE);
         $quoteCode = $this->request->request->get('quoteCode', null);
 
-        $prevDayDate = date(CBRClient::CBR_DATE_FORMAT, strtotime($date . '- 1 day'));
+        $prevDayDate = date(CBRClient::CBR_DATE_FORMAT, strtotime($date . '- ' . self::MAX_DAYS_RANGE . ' day'));
+
         $baseRates = $this->CBRClient->getDynamicRate($prevDayDate, $date, $baseCode);
         $quoteRates = $this->CBRClient->getDynamicRate($prevDayDate, $date, $quoteCode);
-        $prevCrossRate = (float)$baseRates[0] / (float)$quoteRates[0];
-        $crossRate = (float)$baseRates[1] / (float)$quoteRates[1];
 
-        $response = [
-            'data' => [
-                'currentCrossRate' => $crossRate,
-                'diffToPrevDayCrossRate' => number_format($crossRate - $prevCrossRate, 8),
-            ]
-        ];
+        if ($baseRates && $quoteRates) {
+            $prevCrossRate = (float)$baseRates[0] / (float)$quoteRates[0];
+            $crossRate = (float)$baseRates[1] / (float)$quoteRates[1];
+
+            $response = [
+                'data' => [
+                    'currentCrossRate' => $crossRate,
+                    'diffToPrevDayCrossRate' => number_format($crossRate - $prevCrossRate, 8),
+                ]
+            ];
+        } else {
+            $this->responceStatus = Response::HTTP_BAD_REQUEST;
+            $response = ['error' => "Cannnot find currency"];
+        }
         echo(new JsonResponse($response, $this->responceStatus));
+
+
     }
 
 
